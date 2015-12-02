2ceb0857bcded05bb3a74084be763b612c911aee
Bruno Dutertre <bruno@csl.sri.com>
2015-12-01 18:27:15 -0800

    More test: trying harder to exercise malloc

--- dsmalloc/mtest2.c
+++ dsmalloc/mtest2.c
@@ -14,7 +14,7 @@
 int main(void) {
   char **bank;
   char *test;
-  uint32_t i;
+  uint32_t i, step;
   size_t sz;
 
   bank = (char **) malloc(BNK_SIZE * sizeof(char*));
@@ -24,39 +24,54 @@ int main(void) {
     return 1;
   }
 
-  sz = 5;
-  for (i=0; i<BNK_SIZE; i++) {
-    test = (char *) malloc(sz);
-    if (test == NULL) {
-      fprintf(stderr, "malloc(%zu) failed (i = %"PRIu32")\n", sz, i);
-      return 1;
+  for (sz = 5; sz < 100000; sz *= 4) {
+    for (i=0; i<BNK_SIZE; i++) {
+      test = (char *) malloc(sz);
+      if (test == NULL) {
+	fprintf(stderr, "malloc(%zu) failed (i = %"PRIu32")\n", sz, i);
+	return 1;
+      }
+      bank[i] = test;
     }
-    bank[i] = test;
-  }
-  printf("Allocated %"PRIu32" blocks of size %lu\n", i, (unsigned long) sz);
+    printf("Allocated %"PRIu32" blocks of size %lu\n", i, (unsigned long) sz);
 
-  for (i=0; i<BNK_SIZE; i++) {
-    free(bank[i]);
-    bank[i] = NULL;
+    for (i=0; i<BNK_SIZE; i++) {
+      free(bank[i]);
+      bank[i] = NULL;
+    }
   }
   
-  printf("Doing it again\n");
-
-  for (i=0; i<BNK_SIZE; i++) {
-    test = (char *) malloc(sz);
-    if (test == NULL) {
-      fprintf(stderr, "malloc(%zu) failed (i = %"PRIu32")\n", sz, i);
-      return 1;
+  for (sz = 1000000; sz > 10; sz /= 3) {
+    for (i=0; i<BNK_SIZE; i++) {
+      test = (char *) realloc(bank[i], sz);
+      if (test == NULL) {
+	fprintf(stderr, "malloc(%zu) failed (i = %"PRIu32")\n", sz, i);
+	return 1;
+      }
+      bank[i] = test;
     }
-    bank[i] = test;
+    printf("Reallocated %"PRIu32" blocks of size %lu\n", i, (unsigned long) sz);
   }
-  printf("Allocated %"PRIu32" blocks of size %lu\n", i, (unsigned long) sz);
+
+  for (step = 2; step < 5; step ++) {
+    for (i=0; i<BNK_SIZE; i+=step) {
+      sz = (size_t) (random() % 2000000);
+      if (bank[i] != NULL) {
+	free(bank[i]);
+      }
+      test = (char *) malloc(sz);
+      if (test == NULL) {
+	fprintf(stderr, "malloc(%zu) failed (i = %"PRIu32")\n", sz, i);
+	return 1;
+      }
+      bank[i] = test;
+    }
+  }
 
   for (i=0; i<BNK_SIZE; i++) {
     free(bank[i]);
-    bank[i] = NULL;
   }
-
+  
   free(bank);
 
   malloc_stats();

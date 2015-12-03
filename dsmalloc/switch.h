0aa521f6019875a1240053f52358ec390abbec8b
Ian A Mason <iam@csl.sri.com>
2015-12-03 09:21:03 -0800

    Cleaning up replay a bit ...

---
+++ dsmalloc/switch.h
@@ -0,0 +1,29 @@
+#ifndef _SWITCH_H
+#define _SWITCH_H
+
+
+#ifdef USE_DL_PREFIX
+
+#ifndef HAVE_MMAP
+#define HAVE_MMAP
+#endif
+
+#ifndef _DSMALLOC_H
+#include "dsmalloc.h"
+#else 
+#include <assert.h>
+#endif
+
+
+#define malloc(X)      dlmalloc(X)
+#define calloc(X, Y)   dlcalloc(X, Y)
+#define realloc(X, Y)  dlrealloc(X, Y)
+#define free(X)        dlfree(X)
+#define malloc_stats() dlmalloc_stats()
+#endif
+
+
+
+
+
+#endif

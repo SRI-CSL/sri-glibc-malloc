68e386c3d00e9c23262ea9768a6d328b6f88ddee
Ian A. Mason <iam@csl.sri.com>
2015-12-26 17:27:51 -0800

    working on the mreplay mystery.

---
+++ psmalloc2/mtreplay.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+#include "malloc.h"
+
+#include "replaylib.h"
+
+#define MAX_THREADS  1024
+
+
+/*
+ *  Parses the output from ../mhooks/mhook.c and replays it.
+ *
+ *  Hopefully in the exact same fashion (but whether our calls to
+ *  realloc match the scripts lies in the lap of the malloc gods). 
+ *
+ *  I guess we will be able to check that (modulo callers) when 
+ *  it is finished.
+ *
+ *  By design it is very unforgiving on the input.
+ *
+ *  Keep in mind that mhook.c misses stuff at startup, and that the
+ *  main idea behind the replay script is to trigger similar bugs
+ *  in the client malloc library.
+ *
+ */
+
+typedef struct targs {
+  int id;
+  const char* filename;
+} targs_t;
+
+
+
+
+void* thread_main(void* targ){
+  targs_t* targsp = (targs_t*)targ;
+  process_file(targsp->filename, true);
+  pthread_exit(NULL);
+}
+
+
+int main(int argc, char* argv[]){
+  int nthreads;
+  int rc;
+  int i;
+  pthread_t threads[MAX_THREADS];
+  targs_t targs[MAX_THREADS];
+  void* status;
+  
+  if (argc != 3) {
+    fprintf(stdout, "Usage: %s <nthreads> <mhook output file>\n", argv[0]);
+    return 1;
+  }
+
+  nthreads = atoi(argv[1]);
+
+
+  if(nthreads <= 0){
+
+    process_file(argv[2], true);
+
+
+  } else if ( nthreads >= MAX_THREADS){
+    fprintf(stdout, "nthreads must be less than %d\n", MAX_THREADS);
+    return 1;
+  } else {
+
+   
+   for( i = 0; i < nthreads; i++){
+
+     targs[i].id = i;
+
+     targs[i].filename = argv[2];
+
+     rc = pthread_create(&threads[i], NULL, thread_main, (void *)&targs[i]);
+
+     if (rc){
+       fprintf(stderr, "return code from pthread_create() is %d\n", rc);
+       exit(-1);
+     }
+
+   }
+
+
+   for( i = 0; i < nthreads; i++){
+
+      rc = pthread_join(threads[i], &status);
+        if (rc){
+	  fprintf(stderr, "return code from pthread_join() is %d\n", rc);
+	  exit(-1);
+	}
+	
+   }
+   
+  }
+  
+  malloc_stats();
+  
+  
+  pthread_exit(NULL);
+
+}

e83c822d899523ff80c9d187c5aa88c1fbb8f767
Ian A. Mason <iam@csl.sri.com>
2015-12-27 09:04:19 -0800

    Moving on to the USE_ARENAS case.

--- psmalloc2/mtreplay.c
+++ psmalloc2/mtreplay.c
@@ -11,19 +11,13 @@
 
 
 /*
- *  Parses the output from ../mhooks/mhook.c and replays it.
+ *  Nothing fancy yet.
+ *  Given nthreads and an mhook script it creates
+ *  that number of threads, each replaying the script.
  *
  *  Hopefully in the exact same fashion (but whether our calls to
  *  realloc match the scripts lies in the lap of the malloc gods). 
  *
- *  I guess we will be able to check that (modulo callers) when 
- *  it is finished.
- *
- *  By design it is very unforgiving on the input.
- *
- *  Keep in mind that mhook.c misses stuff at startup, and that the
- *  main idea behind the replay script is to trigger similar bugs
- *  in the client malloc library.
  *
  */
 

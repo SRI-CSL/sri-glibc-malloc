b940297703e1b536d6f8667991da7ac5786ec5b7
Ian A. Mason <iam@csl.sri.com>
2016-04-13 10:05:11 -0700

    lfht is still in the incubator...

--- lfht/include/lfht.h
+++ lfht/include/lfht.h
@@ -19,7 +19,21 @@ typedef struct lfht_s {
   lfht_entry_t *table;
 } lfht_t;
 
+/* 
+  Initializes an uninitialized lfht_t struct.
+  Used like so:
 
+  lfht_t ht;
+  bool success = init_lfht(&ht, 4096);
+  if(success){
+   //off to races
+  
+  }
+ 
+   - max is the maximum number of key value pairs that the table will be able to store. 
+     It is a hard limit.
+
+*/
 extern bool init_lfht(lfht_t *ht, uint64_t max);
 
 extern bool delete_lfht(lfht_t *ht, uint64_t max);
--- lfht/src/lfht.c
+++ lfht/src/lfht.c
@@ -1,3 +1,26 @@
 #include "lfht.h"
 
 
+bool init_lfht(lfht_t *ht, uint64_t max){
+  uint64_t sz;
+  void *addr;
+  if(ht != NULL && max != 0){
+    sz = max * sizeof(lfht_entry_t);
+    if(max < sz){
+      addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+      if (addr != MAP_FAILED) {
+	ht->table = (lfht_entry_t *)addr;
+	ht->max = max;
+	return true;
+      }
+    }
+  }
+  return false;
+}
+
+extern bool delete_lfht(lfht_t *ht, uint64_t max);
+
+extern bool lfht_insert(lfht_t *ht, uintptr_t key, uintptr_t val);
+
+extern bool lfht_find(lfht_t *ht, uintptr_t key, uintptr_t *valp);
+  

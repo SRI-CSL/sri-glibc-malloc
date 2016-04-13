ab8780e00b3c6d49291126dcd03e3c0e25a7236a
Ian A. Mason <iam@csl.sri.com>
2016-04-13 11:09:21 -0700

    still 'n 'bator

---
+++ lfht/arch/x86_64/atomic.h
@@ -0,0 +1,43 @@
+#ifndef __ATOMIC_H__
+#define __ATOMIC_H__
+
+#include <stdint.h>
+
+typedef struct {
+  uint64_t uno;
+  uint64_t dos;
+} u128_t;
+
+
+#define LOCK_PREFIX	"lock ; "
+
+static inline unsigned int cas_64(volatile unsigned long *address, unsigned long old_value, unsigned long new_value)
+{
+	unsigned long prev = 0;
+
+	asm volatile(LOCK_PREFIX "cmpxchgq %1,%2"
+		: "=a"(prev)
+		: "r"(new_value), "m"(*address), "0"(old_value)
+		: "memory");
+
+	return prev == old_value;
+}
+
+static inline unsigned int cas_128(volatile u128_t *address, u128_t old_value, u128_t new_value)
+{
+
+  char result;
+  asm volatile
+    (LOCK_PREFIX "cmpxchg16b %1\n\t"
+     "setz %0\n"
+     : "=q" ( result )
+       , "+m" ( *address )
+     : "a" ( old_value.ptr ), "d" ( old_value.tag )
+       ,"b" ( new_value.ptr ), "c" ( new_value.tag )
+     : "cc"
+     );
+  return result;
+}
+
+#endif
+

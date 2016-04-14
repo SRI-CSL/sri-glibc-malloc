14851197ccca81e9cc0c49a9047afa5b6adfa126
Ian A. Mason <iam@csl.sri.com>
2016-04-13 19:39:20 -0700

    Started hiring in the the testing department.

--- lfht/tests/unit/test_one.c
+++ lfht/tests/unit/test_one.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lfht.h"
 
 
@@ -5,22 +6,35 @@ uint32_t max = 16 * 4096;
 
 static lfht_t ht;
 
+extern bool lfht_insert(lfht_t *ht, uintptr_t key, uintptr_t val);
+
+extern bool lfht_find(lfht_t *ht, uintptr_t key, uintptr_t *valp);
+  
+
 
 int main(int argc, char* argv[]){
-
+  uint32_t i;
   bool success;
 
 
   success = init_lfht(&ht, max);
 
+  if( !success ) exit(EXIT_FAILURE);
 
-
+  for(i = 1; i <= max; i++){
+    if( ! lfht_insert(&ht, i, i) ){
+      fprintf(stderr, "%s failed for i = %d\n", argv[0], i);
+      exit(EXIT_FAILURE);
+    }
+  }
 
 
   success = delete_lfht(&ht);
 
+  if( !success ) exit(EXIT_FAILURE);
 
 
+  exit(EXIT_SUCCESS);
 
 }
 

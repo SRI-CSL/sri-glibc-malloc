eaafe82d8615ee2740256320e7c9232fb8c8de10
Ian A Mason <iam@csl.sri.com>
2016-05-01 15:00:46 -0700

    getting ready to move the queues out of arch.

--- frolloc/arch/x86_64/lifo/lf_queue.h
+++ frolloc/arch/x86_64/lifo/lf_queue.h
@@ -22,51 +22,10 @@ typedef struct {
 
 #define LF_QUEUE_STATIC_INIT  {{0, 0}}
 
-static inline void lf_queue_init(lf_queue_t *queue)
-{
-	queue->both.top = 0;
-	queue->both.count = 0;
-}
+extern void lf_queue_init(lf_queue_t *queue);
 
+extern void *lf_dequeue(lf_queue_t *queue);
 
-static inline void *lf_dequeue(lf_queue_t *queue)
-{
-	top_aba_t head;
-	top_aba_t next;
-
-	while(1) {
-		head.top = queue->both.top;
-		head.count = queue->both.count;
-		if (head.top == 0)
-			return NULL;
-		next.top = (uintptr_t)((struct queue_elem_t *)head.top)->next;
-		next.count = head.count + 1;
-		if (cas_64((volatile uint64_t *)&(queue->both),
-			   *((uint64_t*)&head),
-			   *((uint64_t*)&next))) {
-			return ((void *)head.top);
-		}
-	}
-}
-static inline int lf_enqueue(lf_queue_t *queue, void *element)
-{
-	top_aba_t old_top;
-	top_aba_t new_top;
-	void *next;
-	
-	while(1) {
-		old_top.count = queue->both.count;
-		old_top.top = queue->both.top;
-		next = (void *)old_top.top;
-		((struct queue_elem_t *)element)->next = (struct queue_elem_t *)next;
-		new_top.top = (unsigned long)element;
-		new_top.count = old_top.count + 1;
-		if (cas_64((volatile uint64_t *)&(queue->both),
-			   *((uint64_t *)&old_top),
-			   *((uint64_t *)&new_top))) {
-			return 0;
-		}
-	}
-}
+extern int lf_enqueue(lf_queue_t *queue, void *element);
 
 #endif

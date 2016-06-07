4069743b69c30195f3fdeced896620da4fe7f0bf
Ian A Mason <iam@csl.sri.com>
2016-06-06 18:23:17 -0700

    locking.h tuning.

--- glibc/malloc/locking.h
+++ glibc/malloc/locking.h
@@ -15,7 +15,7 @@ static __thread  int tid = -1;
 
 static inline void LOCK_ARENA(mstate av, int site){
   int self;
-  int index;
+  //int index;
   if(tid == -1){
     tid = catomic_exchange_and_add (&tid_counter, 1);
   }
@@ -24,34 +24,40 @@ static inline void LOCK_ARENA(mstate av, int site){
 #ifdef SRI_VALGRIND
   VALGRIND_HG_MUTEX_LOCK_PRE(&(av->mutex), 0);
 #endif
+
   (void)mutex_lock(&(av->mutex));
+  log_lock_event(LOCK_ACTION, av, av->arena_index, site, tid);
+
 #ifdef SRI_VALGRIND
   VALGRIND_HG_MUTEX_LOCK_POST(&(av->mutex));
 #endif
-  index = owners[av->arena_index];
+  //index = owners[av->arena_index];
   //allow for recursive locking
-  assert((index == 0) || (index == self));
+  //assert((index == 0) || (index == self));
   owners[av->arena_index] = self;
   
-  //log_lock_event(LOCK_ACTION, av, av->arena_index, site, tid);
+
 }
 
 
 static inline void UNLOCK_ARENA(mstate av, int site){
-  int self = tid;
-  int index = owners[av->arena_index];
+  //int self = tid;
+  //int index = owners[av->arena_index];
   //allow for recursive locking
-  assert((index == 0) || (index == self)); 
+  //assert((index == 0) || (index == self)); 
   owners[av->arena_index] = 0;
 #ifdef SRI_VALGRIND
   VALGRIND_HG_MUTEX_UNLOCK_PRE(&(av->mutex));
 #endif
+
   (void)mutex_unlock(&(av->mutex));
+  log_lock_event(UNLOCK_ACTION, av, av->arena_index, site, tid);
+
 #ifdef SRI_VALGRIND
   VALGRIND_HG_MUTEX_UNLOCK_POST(&(av->mutex));
 #endif
 
-  //log_lock_event(UNLOCK_ACTION, av, av->arena_index, site, tid);
+
 }
 
 #else
